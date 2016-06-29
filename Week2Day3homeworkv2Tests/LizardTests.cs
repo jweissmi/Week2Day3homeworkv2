2d86be691c0d925041c507c4cd9c1536c2896399
Jim Weiss <jweiss@motor.com>
2016-06-28 23:02:24 -0400

    All Tests Working

---
+++ Week2Day3homeworkv2Tests/LizardTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Week2Day3homeworkv2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week2Day3homeworkv2.Tests
+{
+    [TestClass()]
+    public class LizardTests
+    {
+        [TestMethod()]
+        public void LizardTest()
+        {
+            Lizard myTestLizard = new Lizard();
+            Assert.IsNull(myTestLizard.Name);
+            Assert.AreEqual(null, myTestLizard.Name);
+            myTestLizard.Name = "Larry";
+            Assert.AreEqual("Larry", myTestLizard.Name);
+            Assert.AreEqual(null, myTestLizard.Species);
+            myTestLizard.Species = "Lounge Lizard";
+            Assert.AreEqual("Lounge Lizard", myTestLizard.Species);
+            Assert.AreEqual(null, myTestLizard.ExternalCovering);
+            myTestLizard.ExternalCovering = "Scales";
+            Assert.AreEqual("Scales", myTestLizard.ExternalCovering);
+        }
+
+        [TestMethod()]
+        public void IsDesertDwellerTest()
+        {
+            Lizard myTestLizard = new Lizard();
+            Assert.AreEqual(true, myTestLizard.IsDesertDweller()); 
+        }
+    }
+}
---
+++ Week2Day3homeworkv2Tests/SnakeTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Week2Day3homeworkv2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week2Day3homeworkv2.Tests
+{
+    [TestClass()]
+    public class SnakeTests
+    {
+        [TestMethod()]
+        public void SnakeTest()
+        {
+            Snake MyTestSnake = new Snake();
+            Assert.IsNull(MyTestSnake.Name);
+            Assert.AreEqual(null, MyTestSnake.Name);
+            MyTestSnake.Name = "Sam";
+            Assert.AreEqual("Sam", MyTestSnake.Name);
+            Assert.AreEqual(null, MyTestSnake.Species);
+            MyTestSnake.Species = "Rattler";
+            Assert.AreEqual("Rattler", MyTestSnake.Species);
+            Assert.AreEqual(null, MyTestSnake.ExternalCovering);
+            MyTestSnake.ExternalCovering = "Scales";
+            Assert.AreEqual("Scales", MyTestSnake.ExternalCovering);
+        }
+
+        [TestMethod()]
+        public void IsVenomousTest()
+        {
+            Snake MyTestSnake = new Snake();
+            Assert.AreEqual(true, MyTestSnake.IsVenomous());
+        }
+    }
+}
